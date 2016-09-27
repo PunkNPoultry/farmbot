81cdffbe326360d6f63131faef9a21639147cc21
Chris Mason <chris@punknpoultry.com>
2016-09-27 18:20:41 -0500

    Add redirect to homepage

--- farmbot/urls.py
+++ farmbot/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^farm/', include('bot.urls'))
+    url(r'^farm/', include('bot.urls')),
+    url(r'^$', RedirectView.as_view(url='http://punknpoultry.com'), name='go-to-pnp')
 ]
